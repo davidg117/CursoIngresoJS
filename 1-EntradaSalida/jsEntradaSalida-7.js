24715f14292146a8372239a1ae15bebb36d871de
davidg117 <60674880+davidg117@users.noreply.github.com>
2020-02-05 11:56:37 -0300

    ejercicio 7 practica
    sumar solamente(en proceso)

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,9 +2,21 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	
+    var primernumero;
+    var segundonumero;
+    var resultado;
+
+    primernumero=document.getElementById('numeroUno').value;
+    segundonumero=document.getElementById('numeroDos').value;
+   
+    primernumero=parseInt(primernumero);
+    segundonumero=parseInt(segundonumero);
+    resultado=primernumero+segundonumero;
+    console.log(resultado);
+    
 }
 
 function restar()
