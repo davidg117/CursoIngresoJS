978cd7832d1888905bcc89ba96e0de5c8c74427e
davidg117 <60674880+davidg117@users.noreply.github.com>
2020-02-11 12:12:55 -0300

    tp ff
    in process

---
+++ jsFerreteFacturacion.js
@@ -0,0 +1,56 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*///promedio=(A+B+C)/3
+function Sumar () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var total;
+
+	precio1=document.getElementById('PrecioUno').value;
+	precio2=document.getElementById('PrecioDos').value;
+	precio3=document.getElementById('PrecioTres').value;
+
+	precio1=parseInt(precio1);
+	precio2=parseInt(precio2);
+	precio3=parseInt(precio3);
+
+	total=precio1+precio2+precio3;
+	alert("total="+total);
+
+	
+}
+function Promedio () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	var romedio;
+
+	precio1=document.getElementById('PrecioUno').value;
+	precio2=document.getElementById('PrecioDos').value;
+	precio3=document.getElementById('PrecioTres').value;
+
+	precio1=parseInt(precio1);
+	precio2=parseInt(precio2);
+	precio3=parseInt(precio3);
+
+	suma= precio1+precio2+precio3;
+	promedio=suma/3;
+
+	alert("el promedio es:"+promedio);
+
+
+
+	
+	
+}
+function PrecioFinal () 
+{
+	
+	
+}
