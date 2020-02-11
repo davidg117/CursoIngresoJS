a26ae079ee265a0d26a4d26c0900a6d668c3ebb3
davidg117 <60674880+davidg117@users.noreply.github.com>
2020-02-10 22:58:25 -0300

    Exercise 10
    Ahora completado

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -2,14 +2,22 @@
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-//sueldo-sueldo*(descuento/100)
 function mostrarAumento()
 {
 	var sueldo;
+	var porcentaje;
+	var formulaporcent;
 	var descuento;
 	var total;
 
+	sueldo=document.getElementById('importe').value;
+	sueldo=parseInt(sueldo);
 
+	porcentaje=25;
+	formulaporcent=porcentaje/100;
+	descuento=sueldo*formulaporcent;
+	total=sueldo-descuento;
 
-
+	alert("el total es:"+total);
+	
 }
