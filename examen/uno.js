a782159f675d2d422eeb53ee31ce319f3eb020c6
casa <davidg117>
2020-03-10 00:03:30 -0300

    examen repaso 2

---
+++ examen/uno.js
@@ -0,0 +1,41 @@
+function mostrar()
+{
+	var 
+
+
+
+
+
+
+
+
+}
+	/*var producto;
+	var marca;
+	var fabricante;
+	var cantidad;
+	var precio;
+	var contador;
+
+	do
+	{
+		do
+		{
+			producto = prompt("Ingrese el producto.");
+		}
+		while(!isNaN(producto) || producto != "barbijo" && producto != "jabon" && producto != "alcohol");
+		do
+		{
+			marca = prompt("Ingrese la marca del producto");
+		}
+		while(!isNaN(marca));
+		do
+		{
+			fabricante = prompt("Ingrese el fabricante");
+		}
+
+
+
+		contador++;		
+	}
+	while(contador < 5);*/
