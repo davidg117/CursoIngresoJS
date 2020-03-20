813e47b5a31adf3b330f7d821f4c79de3ca1a89b
casa <davidg117>
2020-03-20 19:33:58 -0300

    tp ferrete construccion completado

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,58 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var cantidadDeAlambre;
+	var largo;
+	var ancho;
+	var perimetro;
+	var totalHilos;
+
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo);
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+
+	perimetro=2*largo+2*ancho;
+	totalHilos=perimetro*3;
+
+	alert("se debe comprar "+totalHilos+" metros de alambre");
+
 
 }
 function Circulo () 
 {
-	
+	var cantidadDeAlambre;
+	var circunferencia;
+	var radio;
+	var totalHilos;
+
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+
+	circunferencia=2*Math.PI*radio;
+	circunferencia=parseInt(circunferencia);
+	totalHilos=circunferencia*3;
+	totalHilos=parseInt(totalHilos);
+
+	alert("se debe comprar "+totalHilos+" metros de alambre");
+
 }
 function Materiales () 
 {
-	
+	var ancho;
+	var largo;
+	var cantidadCemento;
+	var cantidadCal;
+
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo);
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+
+	cantidadCemento=(largo*ancho)*2;
+	cantidadCal=(largo*ancho)*3;
+
+	alert("Se necesitan "+cantidadCemento+" bolsas de cemento y "+cantidadCal+" bolsas de cal");
+
+
 }
