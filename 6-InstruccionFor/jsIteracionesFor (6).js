5201997013b476eddd6b3b4bab2df1fc1bd7ae16
casa <davidg117>
2020-03-28 18:02:00 -0300

    examen ej1

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,8 +1,22 @@
 function mostrar()
 {
-	
+	var numeroIngresado;
+	var i;
+	var numeroPares;
+
+	numeroIngresado=prompt("Ingrese el numero");
+	numeroIngresado=parseInt(numeroIngresado);
+	numeroPares=0;
+
+	for(i=1;i<numeroIngresado;i++)
+	{
+		if((i%2)==0)
+		{
+
+		}
+	}
 
 
 
 
-}//FIN DE LA FUNCIÓN
+}
--- examen/uno.js
+++ examen/uno.js
@@ -1,6 +1,50 @@
 function mostrar()
 {
-	var 
+	var producto;
+	var marca;
+	var fabricante;
+	var cantidad;
+	var precio;
+	var contador;
+
+	contador=0;
+
+	do
+	{
+		do
+		{
+			producto = prompt("Ingrese el producto."); 
+		}
+		while(!isNaN(producto) || producto != "barbijo" && producto != "jabon" && producto != "alcohol")
+		do
+		{
+			marca = prompt("Ingrese la marca del producto");
+		}
+		while(!isNaN(marca))
+		do
+		{
+			fabricante = prompt("Ingrese el fabricante");
+		}
+		do
+		{
+			precio=prompt("Ingrese precio");
+			precio=parseInt(precio);
+		}while(isNaN(precio) || precio<100 || precio>300)
+		do
+		{
+			cantidad=prompt("Ingrese cantidad");
+			cantidad=parseInt(cantidad);
+		}while(isNaN())
+
+
+
+		contador++;
+	}
+	while(contador < 5);
+
+	if()
+	{
+	}
 
 
 
